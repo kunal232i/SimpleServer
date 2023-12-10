0126f0e3c9f39fbe72fdb8b9df8b4c57e5aee0f2
kunal232i <kingawale23@gmail.com>
2023-12-10 13:07:35 +0000

    test: check vm deploy

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.get("/sayHello", (req, res) => {
   res.json({ message: "Hello User" });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}.`);
 });
